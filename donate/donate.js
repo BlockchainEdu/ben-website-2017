3051f39b1ed1843611140014f7af91ecec0a9ee7
Kelvin Fichter <kef50@case.edu>
2017-12-27 18:19:41 -0500

    Added donate page

---
+++ donate/donate.js
@@ -0,0 +1,7 @@
+$(() => {
+  new QRCode(document.getElementById('qrcode-eth'), '0xfd5e7D9B422b12022d1488710AA7a1d2F40bA0C4');
+  new QRCode(document.getElementById('qrcode-btc'), '1C2gZ5apiqitgXwXVE1RPYgNj3GZHgZNk6');
+  new QRCode(document.getElementById('qrcode-ltc'), 'LbkRGm3byn1qBCuJbjRauqLDqQQG4HTcTg');
+  new QRCode(document.getElementById('qrcode-qtum'), '0x053a2b614b0e3e41b9f12d511de8218cf07221c0');
+  new QRCode(document.getElementById('qrcode-bch'), '14PNpPRms2dqavG163Eh6zZXTquhgwsx5t');
+})
