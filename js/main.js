e05f13c7f397b78f128d7de3376cef0eff01fd5c
Kelvin Fichter <kef50@case.edu>
2017-12-27 21:52:03 -0500

    Fixed side-nav loading issue

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(() => {
-  $(".button-collapse").sideNav();
-  $("#header").load("components/header.html");
-  $("#footer").load("components/footer.html");
+  $("#header").load("/components/header.html");
+  $("#footer").load("/components/footer.html");
 });
